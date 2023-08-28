c9c8f8dc335e3423a90d72eb400ee81676f4f51e
mellejulan <105960561+mellejulan@users.noreply.github.com>
2023-08-28 13:53:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Omegle</title>
+    <style>
+        body {
+            background-color: black;
+        }
+
+        .tekst {
+            font-size: 70px;
+            text-align: center;
+            color: white;
+        }
+    
+        .br {
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="tekst">Welkom bij omegle fotos</h1>
+    <hr class="br">
+    <img src="capture.PNG" alt="omegle" width="200" height="200">
+    <img src="capture2.PNG" alt="omegle" width="200" height="200">
+    <img src="capture3.PNG" alt="omegle" width="200" height="200">
+    <img src="capture4.PNG" alt="omegle" width="200" height="200">
+    <img src="capture5.PNG" alt="omegle" width="200" height="200">
+    <img src="capture6.PNG" alt="omegle" width="200" height="200">
+    <img src="capture7.PNG" alt="omegle" width="200" height="200">
+    <img src="capture8.PNG" alt="omegle" width="200" height="200">
+    <img src="capture9.PNG" alt="omegle" width="200" height="200">
+    <img src="capture10.PNG" alt="omegle" width="200" height="200">
+    <img src="capture11.PNG" alt="omegle" width="200" height="200">
+    <img src="capture12.PNG" alt="omegle" width="200" height="200">
+</body>
+</html>
